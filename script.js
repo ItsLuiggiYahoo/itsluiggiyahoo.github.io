b31ab8e4f78463f84ce30a91acc13c5c85b6ebee
ItsLuiggiYahoo <56616894+ItsLuiggiYahoo@users.noreply.github.com>
2024-01-17 17:18:58 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-var texts = [
-  "Olá!",
-  "Batata frita é muito bom.",
-  "Sim esses textos são aleatórios."
-];
 
-document.getElementById('randomText').value = texts[Math.floor(Math.random()*texts.length)];
